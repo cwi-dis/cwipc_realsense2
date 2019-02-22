f7e6fcb7c1d82a1f4b38f8d9a874b805201f9f2b
Jack Jansen <Jack.Jansen@cwi.nl>
2019-02-22 17:16:02 +0100

    Moving cwipc_source to cwipc_util.

--- include/cwipc_realsense2/api.h
+++ include/cwipc_realsense2/api.h
@@ -13,16 +13,7 @@
 #endif
 
 #ifdef __cplusplus
-class cwipc_source {
-public:
-    virtual ~cwipc_source() {};
-    virtual void free() = 0;
-    virtual cwipc* get() = 0;
-};
 #else
-typedef struct _cwipc_source {
-    int _dummy;
-} cwipc_source;
 #endif
 
 #ifdef __cplusplus
@@ -30,9 +21,6 @@ extern "C" {
 #endif
 
 _CWIPC_REALSENSE2_EXPORT cwipc_source* cwipc_realsense2(char **errorMessage);
-
-_CWIPC_REALSENSE2_EXPORT cwipc* cwipc_source_get(cwipc_source *);
-_CWIPC_REALSENSE2_EXPORT void cwipc_source_free(cwipc_source *);
 
 #ifdef __cplusplus
 };
