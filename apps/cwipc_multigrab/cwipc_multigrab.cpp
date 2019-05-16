bd6d15bbd9f604b94ec56c4911feb07643e285b6
shirsub <shir.sub@gmail.com>
2019-05-16 11:46:40 +0200

    Modified multigrab for faster capture

--- apps/cwipc_multigrab/cwipc_multigrab.cpp
+++ apps/cwipc_multigrab/cwipc_multigrab.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <fstream>
 
-#include "cwipc_util/api.h"
+#include "cwipc_util/api_pcl.h"
 #include "cwipc_realsense2/api.h"
 
 int main(int argc, char** argv)
 {
-    //char *message = NULL;
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " count directory [configfile]" << std::endl;
-        std::cerr << "Creates COUNT pointclouds from a realsense2 camera and stores the PLY files in the given DIRECTORY" << std::endl;
-        return 2;
-    }
-    int count = atoi(argv[1]);
-    char filename[500];
-    char *error = NULL;
-    
+	//char *message = NULL;
+	if (argc < 3) {
+		std::cerr << "Usage: " << argv[0] << " count directory [configfile]" << std::endl;
+		std::cerr << "Creates COUNT pointclouds from a realsense2 camera and stores the PLY files in the given DIRECTORY" << std::endl;
+		return 2;
+	}
+	int count = atoi(argv[1]);
+	char filename[500];
+	char *error = NULL;
+	std::vector<uint64_t> timestamps;
+	std::vector<cwipc_pcl_pointcloud> capturepointclouds;
 	cwipc_tiledsource *generator;
 	if (argc == 3) {
 		generator = cwipc_realsense2(&error);
@@ -23,10 +24,10 @@ int main(int argc, char** argv)
 	else {
 		generator = cwipc_realsense2_ex(argv[3], &error);
 	}
-    if (error) {
-    	std::cerr << argv[0] << ": creating realsense2 grabber failed: " << error << std::endl;
-    	return 1;
-    }
+	if (error) {
+		std::cerr << argv[0] << ": creating realsense2 grabber failed: " << error << std::endl;
+		return 1;
+	}
 	cwipc_tileinfo tif;
 	generator->get_tileinfo(0, &tif, CWIPC_TILEINFO_VERSION);
 	generator->get_tileinfo(1, &tif, CWIPC_TILEINFO_VERSION);
@@ -34,15 +35,23 @@ int main(int argc, char** argv)
 	generator->get_tileinfo(3, &tif, CWIPC_TILEINFO_VERSION);
 	generator->get_tileinfo(4, &tif, CWIPC_TILEINFO_VERSION);
 	int ok = 0;
-    while (count-- > 0 && ok == 0) {
-    	cwipc *pc = generator->get();
-    	snprintf(filename, sizeof(filename), "%s/pointcloud-%lld.ply", argv[2], pc->timestamp());
-    	ok = cwipc_write(filename, pc, &error);
-    }
-    if (ok < 0) {
-    	std::cerr << "Error: " << error << std::endl;
-    	return 1;
-    }
-    return 0;
+	while (count-- > 0 && ok == 0) {
+		cwipc *pc = generator->get();
+		cwipc_pcl_pointcloud pcl_pc = pc->access_pcl_pointcloud();
+		capturepointclouds.push_back(pcl_pc->makeShared());
+		timestamps.push_back(pc->timestamp());
+	}
+	count = atoi(argv[1]);
+	while (count-- > 0 && ok == 0) {
+		snprintf(filename, sizeof(filename), "%s/pointcloud-%lld.ply", argv[2], timestamps.back());
+		ok = cwipc_write(filename, cwipc_from_pcl(capturepointclouds.back(), timestamps.back(), &error), &error);
+		capturepointclouds.pop_back();
+		timestamps.pop_back();
+	}
+	if (ok < 0) {
+		std::cerr << "Error: " << error << std::endl;
+		return 1;
+	}
+	return 0;
 }
 
