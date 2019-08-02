691c9cfec7f786c77979a7bd7bb94400e409c66a
Jack Jansen <Jack.Jansen@cwi.nl>
2019-08-02 12:29:49 +0200

    Ensure capturer is freed during testing even when a test fails

--- python/test_cwipc_realsense2.py
+++ python/test_cwipc_realsense2.py
@@ -5,7 +5,7 @@ import os
 import sys
 import tempfile
 
-if 0:
+if 1:
     # This code can be used to debug the C++ code in XCode:
     # - build for XCode with cmake
     # - build the cwipc_util project
@@ -15,7 +15,7 @@ if 0:
     # - press return to python3.
     import cwipc.realsense2
     cwipc.realsense2._cwipc_realsense2_dll('/Users/jack/src/VRTogether/cwipc_realsense2/build-xcode/lib/Debug/libcwipc_realsense2.dylib')
-    print('Type return after attaching in XCode debugger - ')
+    print('Type return after attaching in XCode debugger (pid=%d) - ' % os.getpid())
     sys.stdin.readline()
 
 #
@@ -28,43 +28,54 @@ class TestApi(unittest.TestCase):
         
     def test_cwipc_realsense2(self):
         """Test that we can grab a realsense2 image"""
-        grabber = cwipc.realsense2.cwipc_realsense2()
-        self.assertFalse(grabber.eof())
-        self.assertTrue(grabber.available(True))
-        pc = grabber.get()
-        self._verify_pointcloud(pc)
-        grabber.free()
-        pc.free()
+        grabber = None
+        pc = None
+        try:
+            grabber = cwipc.realsense2.cwipc_realsense2()
+            self.assertFalse(grabber.eof())
+            self.assertTrue(grabber.available(True))
+            pc = grabber.get()
+            self._verify_pointcloud(pc)
+        finally:
+            if grabber: grabber.free()
+            if pc: pc.free()
 
     def test_cwipc_realsense2_tileinfo(self):
         """Test that we can get tileinfo from a realsense2 grabber"""
-        grabber = cwipc.realsense2.cwipc_realsense2()
-        nTile = grabber.maxtile()
-        self.assertGreaterEqual(nTile, 1)
-        # Assure the non-tiled-tile exists and points nowhere.
-        tileInfo = grabber.get_tileinfo_dict(0)
-        self.assertIn('normal', tileInfo)
-        self.assertIn('camera', tileInfo)
-        self.assertIn('ncamera', tileInfo)
-        self.assertLessEqual(tileInfo['ncamera'], 1)
-        # Test some minimal conditions for other tiles
-        for i in range(1, nTile):
-            tileInfo = grabber.get_tileinfo_dict(i)
-            if i in (1, 2, 4, 8, 16, 32, 64, 128):
-                # These tiles should exist and have a camera ID
-                self.assertIn('normal', tileInfo)
-                self.assertNotEqual(tileinfo['camera'], None)
-        grabber.free()
+        grabber = None
+        try:
+            grabber = cwipc.realsense2.cwipc_realsense2()
+            nTile = grabber.maxtile()
+            self.assertGreaterEqual(nTile, 1)
+            # Assure the non-tiled-tile exists and points nowhere.
+            tileInfo = grabber.get_tileinfo_dict(0)
+            self.assertIn('normal', tileInfo)
+            self.assertIn('camera', tileInfo)
+            self.assertIn('ncamera', tileInfo)
+            self.assertLessEqual(tileInfo['ncamera'], 1)
+            # Test some minimal conditions for other tiles
+            for i in range(1, nTile):
+                tileInfo = grabber.get_tileinfo_dict(i)
+                if i in (1, 2, 4, 8, 16, 32, 64, 128):
+                    # These tiles should exist and have a camera ID
+                    self.assertIn('normal', tileInfo)
+                    self.assertNotEqual(tileinfo['camera'], None)
+        finally:
+            if grabber: grabber.free()
 
     def test_cwipc_realsense2_configfile(self):
         """Test that we can grab a realsense2 image when we pass in a (non-existent) config file"""
-        grabber = cwipc.realsense2.cwipc_realsense2("./nonexistent.xml")
-        self.assertFalse(grabber.eof())
-        self.assertTrue(grabber.available(True))
-        pc = grabber.get()
-        self._verify_pointcloud(pc)
-        grabber.free()
-        pc.free()
+        grabber = None
+        pc = None
+        try:
+            grabber = cwipc.realsense2.cwipc_realsense2("./nonexistent.xml")
+            self.assertFalse(grabber.eof())
+            self.assertTrue(grabber.available(True))
+            pc = grabber.get()
+            self._verify_pointcloud(pc)
+        finally:
+            if grabber: grabber.free()
+            if pc: pc.free()
 
     def _verify_pointcloud(self, pc):
         points = pc.get_points()
