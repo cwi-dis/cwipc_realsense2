8f38db21ea1310ef6f93239a95da3edf3e8df93c
Thomas Röggla <t.roggla@cwi.nl>
2023-09-07 13:53:39 +0200

    updated files in apps/ directory to a consistent coding standard

--- apps/cwipc_rs2grab/cwipc_rs2grab.cpp
+++ apps/cwipc_rs2grab/cwipc_rs2grab.cpp
@@ -10,42 +10,54 @@
 #undef DEBUG_AUXDATA
 #define DEBUG_CONFIG
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     //char *message = NULL;
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " count directory [configfile]" << std::endl;
 		std::cerr << "Creates COUNT pointclouds from a realsense2 camera and stores the PLY files in the given DIRECTORY" << std::endl;
 		std::cerr << "If directory is - then drop the pointclouds on the floor" << std::endl;
+
 		return 2;
     }
+
     int count = atoi(argv[1]);
     char filename[500];
     char *error = NULL;
-    
+
 	cwipc_tiledsource *generator;
 	char *configFile = NULL;
+
 	if (argc == 4) {
 		configFile = argv[3];
 	}
+
 	generator = cwipc_realsense2(configFile, &error, CWIPC_API_VERSION);
+
     if (generator == NULL) {
         std::cerr << argv[0] << ": creating realsense2 grabber failed: " << error << std::endl;
-        if (getenv("CWIPC_REALSENSE2_TESTING") != NULL) return 0; // No failure while running tests, so we can at least test linking, etc.
+
+        if (getenv("CWIPC_REALSENSE2_TESTING") != NULL) {
+            return 0; // No failure while running tests, so we can at least test linking, etc.
+        }
+
         return 1;
     }
+
     if (error) {
         std::cerr << argv[0] << ": warning while creating realsense2 grabber: " << error << std::endl;
     }
+
 #ifdef DEBUG_AUXDATA
     generator->request_auxiliary_data("rgb");
     generator->request_auxiliary_data("depth");
 #endif
+
 #ifdef DEBUG_CONFIG
     size_t configSize = generator->get_config(nullptr, 0);
     char* configBuf = (char*)malloc(configSize + 1);
     memset(configBuf, 0, configSize + 1);
     generator->get_config(configBuf, configSize);
+
     std::cerr << "cameraconfig as json:\n=================\n" << configBuf << "\n======================\n";
 #endif
 
@@ -56,28 +68,39 @@ int main(int argc, char** argv)
 	generator->get_tileinfo(3, &tif);
 	generator->get_tileinfo(4, &tif);
 	int ok = 0;
+
     while (count-- > 0 && ok == 0) {
         cwipc *pc = NULL;
+
         while(1) {
             pc = generator->get();
+
             if (pc == NULL) {
                 error = (char *)"grabber returned NULL";
                 ok = -1;
                 break;
             }
-            if (pc->count() > 0) break;
+
+            if (pc->count() > 0) {
+                break;
+            }
+
             std::cerr << argv[0] << ": warning: empty pointcloud, grabbing again" << std::endl;
         }
+
 		if (strcmp(argv[2], "-") != 0) {
 			snprintf(filename, sizeof(filename), "%s/pointcloud-%" PRIu64 ".ply", argv[2], pc->timestamp());
 			ok = cwipc_write(filename, pc, &error);
 		}
+
 #ifdef DEBUG_AUXDATA
         cwipc_auxiliary_data* ap = pc->access_auxiliary_data();
+
         if (ap == nullptr) {
             std::cerr << argv[0] << ": access_auxiliary_data: returned null pointer" << std::endl;
         } else {
             std::cerr << argv[0] << ": auxdata: " << ap->count() << " items:" << std::endl;
+
             for (int i=0; i<ap->count(); i++) {
                 const char *name = ap->name(i).c_str();
                 const char *description = ap->description(i).c_str();
@@ -89,11 +112,14 @@ int main(int argc, char** argv)
 #endif
         pc->free();
     }
+
     generator->free();
+
     if (ok < 0) {
     	std::cerr << "Error: " << error << std::endl;
     	return 1;
     }
+
     return 0;
 }
 
