8d62582760aa9d67a2e616a63900fed578f75a74
Jack Jansen <Jack.Jansen@cwi.nl>
2026-02-04 17:57:37 +0100

    Fixed the first set of free() references.

--- python/test_cwipc_realsense2.py
+++ python/test_cwipc_realsense2.py
@@ -24,78 +24,64 @@ class TestApi(unittest.TestCase):
         
     def test_cwipc_realsense2(self):
         """Test that we can grab a realsense2 image"""
-        grabber = None
-        pc = None
-        try:
-            grabber = self._open_grabber()
-            self.assertFalse(grabber.eof())
-            self.assertTrue(grabber.available(True))
-            pc = grabber.get()
-            self.assertIsNotNone(pc)
-            assert pc # Only to keep linters happy
-            self._verify_pointcloud(pc)
-        finally:
-            if grabber: grabber.free()
-            if pc: pc.free()
+
+        grabber = self._open_grabber()
+        self.assertFalse(grabber.eof())
+        self.assertTrue(grabber.available(True))
+        pc = grabber.get()
+        self.assertIsNotNone(pc)
+        assert pc # Only to keep linters happy
+        self._verify_pointcloud(pc)
+        grabber.stop()
 
     def test_cwipc_realsense2_tileinfo(self):
         """Test that we can get tileinfo from a realsense2 grabber"""
-        grabber = None
-        try:
-            grabber = self._open_grabber()
-            nTile = grabber.maxtile()
-            self.assertGreaterEqual(nTile, 1)
-            # Assure the non-tiled-tile exists and points nowhere.
-            tileInfo = grabber.get_tileinfo_dict(0)
+        grabber = self._open_grabber()
+        nTile = grabber.maxtile()
+        self.assertGreaterEqual(nTile, 1)
+        # Assure the non-tiled-tile exists and points nowhere.
+        tileInfo = grabber.get_tileinfo_dict(0)
+        self.assertIsNotNone(tileInfo)
+        assert tileInfo # Only to keep linters happy
+        self.assertIn('normal', tileInfo)
+        self.assertIn('cameraName', tileInfo)
+        self.assertIn('cameraMask', tileInfo)
+        self.assertIn('ncamera', tileInfo)
+        # Untrue if multiple realsenses connected: self.assertLessEqual(tileInfo['ncamera'], 1)
+        # Test some minimal conditions for other tiles
+        for i in range(1, nTile):
+            tileInfo = grabber.get_tileinfo_dict(i)
             self.assertIsNotNone(tileInfo)
             assert tileInfo # Only to keep linters happy
-            self.assertIn('normal', tileInfo)
-            self.assertIn('cameraName', tileInfo)
-            self.assertIn('cameraMask', tileInfo)
-            self.assertIn('ncamera', tileInfo)
-            # Untrue if multiple realsenses connected: self.assertLessEqual(tileInfo['ncamera'], 1)
-            # Test some minimal conditions for other tiles
-            for i in range(1, nTile):
-                tileInfo = grabber.get_tileinfo_dict(i)
-                self.assertIsNotNone(tileInfo)
-                assert tileInfo # Only to keep linters happy
-                if i in (1, 2, 4, 8, 16, 32, 64, 128):
-                    # These tiles should exist and have a normal and camera ID (which may be None)
-                    self.assertIn('normal', tileInfo)
-                    self.assertIn('camera', tileInfo)
-        finally:
-            if grabber: grabber.free()
+            if i in (1, 2, 4, 8, 16, 32, 64, 128):
+                # These tiles should exist and have a normal and camera ID (which may be None)
+                self.assertIn('normal', tileInfo)
+                self.assertIn('camera', tileInfo)
+        grabber.stop()
 
     @unittest.skipIf('CI' in os.environ, "Skipping playback test on CI server")
     def test_cwipc_realsense2_playback(self):
         """Test that we can grab a realsense2 image from the playback grabber"""
-        grabber = None
-        pc = None
         if not os.path.exists(TEST_FIXTURES_PLAYBACK_CONFIG):
             self.skipTest(f'Playback config file {TEST_FIXTURES_PLAYBACK_CONFIG} not found')
-        try:
-            grabber = _cwipc_realsense2.cwipc_realsense2_playback(TEST_FIXTURES_PLAYBACK_CONFIG)
-            grabber.start()
-            self.assertFalse(grabber.eof())
+        grabber = _cwipc_realsense2.cwipc_realsense2_playback(TEST_FIXTURES_PLAYBACK_CONFIG)
+        grabber.start()
+        self.assertFalse(grabber.eof())
+        self.assertTrue(grabber.available(True))
+        pc = grabber.get()
+        # Hack around problem: sometimes we get NULL or empty clouds at the beginnging of the run.
+        if pc is None or pc.count() == 0:
+            pc = None
             self.assertTrue(grabber.available(True))
             pc = grabber.get()
-            # Hack around problem: sometimes we get NULL or empty clouds at the beginnging of the run.
-            if pc is None or pc.count() == 0:
-                if pc != None:
-                    pc.free()
-                self.assertTrue(grabber.available(True))
-                pc = grabber.get()
-            if pc is None or pc.count() == 0:
-                if pc != None:
-                    pc.free()
-                self.assertTrue(grabber.available(True))
-                pc = grabber.get()
-            self.assertIsNotNone(pc)
-            assert pc # Only to keep linters happy
-            self._verify_pointcloud(pc)
-        finally:
-            if grabber: grabber.free()
-            if pc: pc.free()
+        if pc is None or pc.count() == 0:
+            pc = None
+            self.assertTrue(grabber.available(True))
+            pc = grabber.get()
+        self.assertIsNotNone(pc)
+        assert pc # Only to keep linters happy
+        self._verify_pointcloud(pc)
+        grabber.stop()
 
     def _verify_pointcloud(self, pc : cwipc.cwipc_pointcloud_wrapper):
         points = pc.get_points()
